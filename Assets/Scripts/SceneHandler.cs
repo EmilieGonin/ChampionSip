2cc5ff347e73a32016db0b4305c10eaaa43405d7
Emilie Gonin <emilie.yuu@gmail.com>
2024-06-30 22:08:12 +0200

    Initial commit

---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,36 @@
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    [HorizontalLine(color: EColor.Green)]
+    [BoxGroup("Load Settings"), SerializeField, Scene] private string _sceneToLoad;
+    [BoxGroup("Load Settings"), SerializeField] private bool _loadAsAdditive;
+    [BoxGroup("Load Settings"), SerializeField] private bool _instantLoad;
+
+    [HorizontalLine(color: EColor.Red)]
+    [BoxGroup("Unload Settings"), SerializeField] private bool _unloadCurrentScene = true;
+    [BoxGroup("Unload Settings"), HideIf("_unloadCurrentScene"), SerializeField, Scene]
+    private string _sceneToUnload;
+
+    private LoadSceneMode _loadSceneMode;
+
+    private void Awake()
+    {
+        _loadSceneMode = _loadAsAdditive ? LoadSceneMode.Additive : LoadSceneMode.Single;
+        if (!_instantLoad) return;
+        Load();
+    }
+
+    public void Load()
+    {
+        SceneManager.LoadScene(_sceneToLoad, _loadSceneMode);
+    }
+
+    public void Unload()
+    {
+        string scene = _unloadCurrentScene ? gameObject.scene.name : _sceneToUnload;
+        SceneManager.UnloadSceneAsync(scene);
+    }
+}
