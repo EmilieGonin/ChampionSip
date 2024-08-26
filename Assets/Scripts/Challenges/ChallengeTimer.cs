57dba065fa00cf7f178ea112cd725e25e6173929
Emilie Gonin <emilie.yuu@gmail.com>
2024-08-26 13:21:13 +0200

    add challenge timer

---
+++ Assets/Scripts/Challenges/ChallengeTimer.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChallengeTimer : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _timer;
+    [SerializeField] private Button _timerButton;
+    [SerializeField] private Image _timerIcon;
+
+    public void StartTimer() => StartCoroutine(Timer());
+
+    private IEnumerator Timer()
+    {
+        _timerButton.interactable = false;
+        _timerIcon.color = _timerButton.colors.disabledColor;
+
+        TimeSpan time = new(0, 5, 0);
+        TimeSpan tick = new(0, 0, 1);
+
+        while (time.TotalSeconds > 0)
+        {
+            //Debug.Log(time.ToString());
+            time = time.Subtract(tick);
+            _timer.text = time.ToString();
+            yield return new WaitForSeconds(1);
+        }
+
+        _timer.text = "Défi disponible";
+        _timerButton.interactable = true;
+        _timerIcon.color = Color.white;
+        yield return null;
+    }
+}
